68f038ab62e5b40f0d905663d1cbf628280b9783
Andrii Olkhovych <hello@def4alt.com>
2024-11-26 14:27:48 +0100

    Change colorscheme. Fix oil.nvim on nvim startup

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -13,5 +13,30 @@ return {
       default_file_explorer = true,
     },
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    init = function(p)
+      if vim.fn.argc() == 1 then
+        local argv = tostring(vim.fn.argv(0))
+        local stat = vim.loop.fs_stat(argv)
+
+        local remote_dir_args = vim.startswith(argv, "ssh")
+          or vim.startswith(argv, "sftp")
+          or vim.startswith(argv, "scp")
+
+        if stat and stat.type == "directory" or remote_dir_args then
+          require("lazy").load({ plugins = { p.name } })
+        end
+      end
+      if not require("lazy.core.config").plugins[p.name]._.loaded then
+        vim.api.nvim_create_autocmd("BufNew", {
+          callback = function()
+            if vim.fn.isdirectory(vim.fn.expand("<afile>")) == 1 then
+              require("lazy").load({ plugins = { "oil.nvim" } })
+              -- Once oil is loaded, we can delete this autocmd
+              return true
+            end
+          end,
+        })
+      end
+    end,
   },
 }
